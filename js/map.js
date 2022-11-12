5792690ed58ef30c4c5ce1badf78ca92acaf9b9c
audreymiller12 <u1197148@utah.edu>
2022-11-11 21:41:27 -0700

    finalized towers

--- js/map.js
+++ js/map.js
@@ -6,16 +6,7 @@ class GondolaMap {
         this.towerData = globalAppState.towerData
         this.boulderData = globalAppState.boulderData
 
-
-        const mapSelection = d3.select("#map")
-        // draw initial map with bounds
-        var map = this.drawInitMap(mapSelection)
-
-        // get initial bounds to use in drawing towers to know where to draw
-        google.maps.event.addListenerOnce(map, 'tilesloaded', () => {
-            var bounds = map.getBounds()
-            this.drawTowers(bounds, 13, map)
-        })
+        this.drawInitMap(d3.select("#map"))
 
     }
 
@@ -26,6 +17,7 @@ class GondolaMap {
      */
     drawInitMap(selection) {
 
+        // Styling to remove some of the google maps interest points
         var styling = [
             {
                 featureType: "poi",
@@ -33,13 +25,12 @@ class GondolaMap {
                 stylers: [
                     { visibility: "off" }
                 ],
-
             }
         ];
 
         // initialize map
         var map = new google.maps.Map(selection.node(), {
-            zoom: 13,
+            zoom: 12.75,
             center: new google.maps.LatLng(40.574215, -111.715113),
             mapTypeId: google.maps.MapTypeId.TERRAIN,
             zoomControl: true,
@@ -50,64 +41,55 @@ class GondolaMap {
 
         // set on zoom changed listener to update data
         map.addListener('zoom_changed', () => {
-            this.drawTowers(map.getBounds(), map.getZoom(), map)
+            // this.drawTowers(map.getBounds(), map.getZoom(), map) 
         })
-        return map
+
+        this.drawTowers(this.towerData, map)
     }
 
 
-    drawTowers(bounds, zoom, map) {
+    drawTowers(data, map) {
 
-        var currTowerData = this.towerData.filter(function (d) {
-            return d.long > bounds.Ha.lo && d.long < bounds.Ha.hi
-        })
+        // get an overlay layer to draw d3 elements onto
+        const mapOverlay = new google.maps.OverlayView()
 
-        // var currTowerData = this.boulderData.filter(function (d) {
-        //     if(d.long && d.lat){
-        //         return d.long > bounds.Ha.lo && d.long < bounds.Ha.hi
-        //     }else if(d.children.long && d.children.lat){
-        //         return d.children.long > bounds.Ha.lo && d.children.long < bounds.Ha.hi
-        //     }
-        // })
+        mapOverlay.onAdd = function () {
+            // overlayLayer doesn't receive DOM events
+            // overlayMouseTarget receives DOM events
+            const mapDiv = d3.select(this.getPanes().overlayMouseTarget).append('div')
 
-        var width = parseInt(d3.select("#map").style("width"))
-        var height = parseInt(d3.select("#map").style("height"))
+            mapOverlay.draw = () => {
 
-        const overlay = new google.maps.OverlayView()
+                // get current projection so lat and long can be converted to x and y coords
+                var projection = this.getProjection()
 
-        overlay.onAdd = function () {
-            const layer = d3.select(this.getPanes().overlayLayer).append('div').style('position', 'absolute')
-                .attr('id', 'overlay')
-
-            overlay.draw = function () {
-                var projection = overlay.getProjection()
-                var svg = layer.selectAll('svg')
-                    .data(currTowerData)
+                //  create an svg for each tower to plot the rect onto
+                var towerSvgs = mapDiv.selectAll('svg')
+                    .data(data)
                     .join('svg')
-                    .attr('width', 10)
-                    .attr('height', 10)
-                    .attr('transform', d => 'translate('+ (convertToXY(d).x) + ',' + (convertToXY(d).y)+ ')')
-
-
-                svg.append('rect')
+                    .style('left', d => calcXY(d).x + 'px')
+                    .style('top', d => calcXY(d).y + 'px')
+                    .attr("class", "mapsvg")
+            
+                // append rectangles to the svg's
+                towerSvgs.append('rect')
                     .attr('width', 10)
                     .attr('height', 10)
                     .attr('class', 'tower')
+                    .on("mouseover", function (event, d) {
+                        console.log('mouse over tower')
+                    })
 
 
-                   
-                function convertToXY(d) {
-                    d = projection.fromLatLngToDivPixel(new google.maps.LatLng(d.lat,d.long))  
-
-                    var coords = {
-                        x: d.x,
-                        y: d.y
-                    }
-                    return coords
+                // convert the lat and long coordinates to x and y coordinates
+                function calcXY(d){ 
+                    d = projection.fromLatLngToDivPixel(new google.maps.LatLng(d.lat, d.long))
+                    return {x : d.x, y : d.y}
                 }
+
             }
         }
-        overlay.setMap(map)
+        mapOverlay.setMap(map)
 
     }
 
