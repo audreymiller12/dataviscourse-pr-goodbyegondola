67e91f833f400bfd1d1fd764375335bdec2b916b
audreymiller12 <u1197148@utah.edu>
2022-11-13 14:38:34 -0700

    adding a legend

--- js/map.js
+++ js/map.js
@@ -9,11 +9,73 @@ class GondolaMap {
 
         this.affectedBoulders = this.filterAffectedBoulders()
 
+        this.climbingAreas = this.filterClimbingAreas()
+
         this.drawInitMap(d3.select("#map"))
+        this.drawLegend(d3.select('#map'))
 
     }
 
+    drawLegend(map){
 
+        var legendDiv = map.append('div')
+            .style('width', '175px')
+            .style('height', '200px')
+            .attr('class', 'legend')
+
+        var legend = legendDiv.append('svg')
+            .attr('width', 175)
+            .attr('class', 'mapsvg')
+            .attr('height', 200)
+            .attr('id', 'legend')
+            
+
+        legend
+            .append('text')
+            .text('Legend: ')
+            .attr('class', 'h1')
+            .attr('x', 10)
+            .attr('y', 20)
+        
+        var gondolaG = legend.append('g')
+            .attr('width', 165)
+            .attr('height', 20)
+            .attr('top', 30)
+            .attr('left', 5)
+        
+        gondolaG
+            .append('rect')
+            .attr('class', 'tower')
+            .attr('top', 15)
+            .attr('left', 40)
+            .attr('width', 10)
+            .attr('height', 10)
+        
+            
+        
+        gondolaG
+            .append('text')
+            .text('- Gondola Tower')
+            .attr('class', 'h1')
+            .attr('x', 40)
+            .attr('y', 40)
+        
+            
+    }
+
+    filterClimbingAreas(){
+        // get all of the children of the main areas
+        var children = this.boulderData.children
+        var childrenAreas = []
+        children.forEach(child => {
+            if (child.children) {
+               childrenAreas.push(child)
+            }
+
+        })
+
+        return children
+    }
     /**
      * 
      * @returns This method returns the affected boulders from the boulder data
@@ -82,7 +144,7 @@ class GondolaMap {
         map.addListener('zoom_changed', () => {
             // this.drawTowers(map.getBounds(), map.getZoom(), map) 
         })
-
+        this.drawArea(this.climbingAreas, map)
         this.drawTowers(this.towerData,map)
         this.drawBoulders(this.affectedBoulders, map)
     }
@@ -96,7 +158,7 @@ class GondolaMap {
         mapOverlay.onAdd = function () {
             // overlayLayer doesn't receive DOM events
             // overlayMouseTarget receives DOM events
-            const mapDiv = d3.select(this.getPanes().overlayMouseTarget).append('div')
+            const mapDiv = d3.select(this.getPanes().overlayMouseTarget).append('div').attr('id', 'towerDiv')
 
             mapOverlay.draw = () => {
 
@@ -152,16 +214,64 @@ class GondolaMap {
                     .join('svg')
                     .style('left', d => calcXY(d).x + 'px')
                     .style('top', d => calcXY(d).y + 'px')
+                    .attr('width', 15)
+                    .attr('height', 15)
                     .attr("class", "mapsvg")
 
                 // append rectangles to the svg's
                 boulderSvgs.append('circle')
-                    .attr('r', 10)
-                    .attr('cx', 5)
-                    .attr('cy', 5)
+                    .attr('r', 7)
+                    .attr('cx', 7.5)
+                    .attr('cy', 7.5)
                     .attr('class', 'boulder')
                     .on("mouseover", function (event, d) {
                         console.log('mouse over boulder')
+                    })
+
+
+                // convert the lat and long coordinates to x and y coordinates
+                function calcXY(d) {
+                    d = projection.fromLatLngToDivPixel(new google.maps.LatLng(d.lat, d.long))
+                    return { x: d.x, y: d.y }
+                }
+
+            }
+        }
+        mapOverlay.setMap(map)
+
+    }
+
+    drawArea(climbingAreas, map) {
+
+        // get an overlay layer to draw d3 elements onto
+        const mapOverlay = new google.maps.OverlayView()
+
+        mapOverlay.onAdd = function () {
+            // overlayLayer doesn't receive DOM events
+            // overlayMouseTarget receives DOM events
+            const mapDiv = d3.select(this.getPanes().overlayMouseTarget).append('div')
+
+            mapOverlay.draw = () => {
+
+                // get current projection so lat and long can be converted to x and y coords
+                var projection = this.getProjection()
+
+                var areaSvgs = mapDiv.selectAll('svg')
+                    .data(climbingAreas)
+                    .join('svg')
+                    .style('left', d => calcXY(d).x + 'px')
+                    .style('top', d => calcXY(d).y + 'px')
+                    .attr('width', 30)
+                    .attr('height', 20)
+                    .attr("class", "mapsvg")
+
+                // append rectangles to the svg's
+                areaSvgs.append('rect')
+                    .attr('width', 30)
+                    .attr('height', 20)
+                    .attr('class', 'area')
+                    .on("mouseover", function (event, d) {
+                        console.log('mouse over area')
                     })
 
 
